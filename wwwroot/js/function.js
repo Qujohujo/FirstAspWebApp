7cf31e185a1c48cc3ab3790ab0a0d709665133f7
Qujohujo <quinnjpete@gmail.com>
2021-01-17 10:59:45 -0700

    Add project files.

---
+++ wwwroot/js/function.js
@@ -0,0 +1,54 @@
+﻿function calcGrade() {
+    let fAssignment = document.getElementById('assignment').value;
+    let fProject = document.getElementById('project').value;
+    let fQuiz = document.getElementById('quiz').value;
+    let fExam = document.getElementById('exam').value;
+    let fIntex = document.getElementById('intex').value;
+
+    let fFinalGrade = 0;
+    let sLetterGrade = '';
+    fFinalGrade = (fAssignment * 0.5) + (fProject * 0.1) + (fQuiz * 0.1) + (fExam * 0.2) + (fIntex * 0.1);
+    fFinalGrade = +fFinalGrade.toFixed(2)
+
+
+    //calculate letter grade from overall percentage
+    if (fFinalGrade >= 94) {
+        sLetterGrade = 'A';
+    }
+    else if (fFinalGrade >= 90) {
+        sLetterGrade = 'A-';
+    }
+    else if (fFinalGrade >= 87) {
+        sLetterGrade = 'B+';
+    }
+    else if (fFinalGrade >= 84) {
+        sLetterGrade = 'B';
+    }
+    else if (fFinalGrade >= 80) {
+        sLetterGrade = 'B-';
+    }
+    else if (fFinalGrade >= 77) {
+        sLetterGrade = 'C+';
+    }
+    else if (fFinalGrade >= 74) {
+        sLetterGrade = 'C';
+    }
+    else if (fFinalGrade >= 70) {
+        sLetterGrade = 'C-';
+    }
+    else if (fFinalGrade >= 67) {
+        sLetterGrade = 'D+';
+    }
+    else if (fFinalGrade >= 64) {
+        sLetterGrade = 'D';
+    }
+    else if (fFinalGrade >= 60) {
+        sLetterGrade = 'D-';
+    }
+    else {
+        sLetterGrade = 'E';
+    }
+
+    //print results to the html dom
+    document.getElementById('outputGrade').innerHTML = 'Your final grade is ' + fFinalGrade + '. You received: ' + sLetterGrade + '.'
+}
